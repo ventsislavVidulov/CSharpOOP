946a185a16377be20d174eabf14331fa195b7d45
ventsislavVidulov <xcen3k@gmail.com>
2025-06-20 15:38:47 +0300

    Create IItem interface

--- InterfacesAndInheritance/Interfaces/IItem.cs
+++ InterfacesAndInheritance/Interfaces/IItem.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InterfacesAndInheritance.Interfaces
+﻿namespace InterfacesAndInheritance.Interfaces
 {
     internal interface IItem
     {
+        // 1. Item Interface(15 points):
+        // Define methods for getting item details, calculating value, and displaying the
+        // item's description.
+
+        string GetItemDetails();
+
+        decimal CalculateValue();
+
+        void DisplayDescription();  
     }
 }
