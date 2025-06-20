2f8fb85d1985f81fd28d332bf890d64fddd0cd90
ventsislavVidulov <xcen3k@gmail.com>
2025-06-20 19:09:40 +0300

    Add ISellable interface

---
+++ InterfacesAndInheritance/Interfaces/ISellable.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace InterfacesAndInheritance.Interfaces
+{
+    internal interface ISellable
+    {
+        // 5. Sellable Interface(10 points):
+        // Create a Sellable interface to represent items that can be sold.
+        // Include methods for setting and getting item prices.
+
+        void SetPrice(decimal price);
+
+        decimal GetPrice();
+
+    }
+}
