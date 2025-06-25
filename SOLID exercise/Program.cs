bf3c058891ec25b761611f744f49b736435a9717
ventsislavVidulov <xcen3k@gmail.com>
2025-06-25 15:50:19 +0300

    initialize SOLID branch

---
+++ SOLID exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
